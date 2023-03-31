0092968c20105f18eee7107eeddfdbc99ee4fca9
dimorphik <dimorphik@gmail.com>
2023-03-31 00:46:16 -0700

    added some responsive stuff

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -7,7 +7,7 @@ const Testimonials = () => {
           <div className="rating">Rating</div>
           <div className="reviewer-details">
             <img src="/assets/images/thumbnail5.webp" alt="thumbnail" />
-            <span>Debbie Culling</span>
+            <span>Lucia Greco</span>
           </div>
           <div className="review-content">
             "Ay, this-a spaghetti alla carbonara, it'sa so good, it make-a my mamma cry!"
@@ -17,31 +17,30 @@ const Testimonials = () => {
           <div className="rating">Rating</div>
           <div className="reviewer-details">
             <img src="/assets/images/thumbnail2.webp" alt="thumbnail" />
-            <span>Doug Lemawn</span>
+            <span>Carmine Ferraro</span>
           </div>
           <div className="review-content">
-            “The mozzarella isa like a fluffy cloud and the basil isa like a spicy rainbow.
-            Fantastico!"
+            “The mozzarella isa like a fluffy cloud and the basil isa like a spicy rainbow!"
           </div>
         </li>
         <li>
           <div className="rating">Rating</div>
           <div className="reviewer-details">
             <img src="/assets/images/thumbnail3.webp" alt="thumbnail" />
-            <span>Gretchen Sklarsky</span>
+            <span>Antonella Lombardi</span>
           </div>
           <div className="review-content">
-            "The bruschetta, it'sa like a party in my mouth and everybody gonna be invited!"
+            "The bruschetta, it'sa like a party ina my mouth and everybody gonna be invited!"
           </div>
         </li>
         <li>
           <div className="rating">Rating</div>
           <div className="reviewer-details">
             <img src="/assets/images/thumbnail4.webp" alt="thumbnail" />
-            <span>Josh Rossman</span>
+            <span>Vincenzo DeLuca</span>
           </div>
           <div className="review-content">
-            "The cannoli, it'sa like a sweeta little bambino ina my hand!"
+            "Disa cannoli, it'sa like a sweeta little bambino ina my hand!"
           </div>
         </li>
       </ul>
